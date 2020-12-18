41f2993bfb6b4a99c2e2605f09a08f6055f77554
gurdeepsingh13 <71901112+gurdeepsingh13@users.noreply.github.com>
2020-12-19 01:16:04 +0530

    Initial commit

---
+++ CalculatorApplication/Models/Calculator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CalculatorApplication.Models
+{
+    public class Calculator
+    {
+        public double firstNumber { get; set; }
+        public double secondNumber { get; set; }
+        public double result { get; set; }
+        public string message { get; set; }
+    }
+}
